e8d601f34cdf6ef7efb870bfe7ca279aed9e546f
Leandro <leanndrocabral@gmail.com>
2023-03-23 19:03:46 -0300

    create backend api

--- app/src/pages/api/login.ts
+++ app/src/pages/api/login.ts
@@ -1,27 +1,27 @@
-import jwt from "jsonwebtoken";
-import { compare } from "bcryptjs";
-import { prisma } from "../../db/db";;
-import { Client } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+// import jwt from "jsonwebtoken";
+// import { compare } from "bcryptjs";
+// import { prisma } from "../../db/db";;
+// import { Client } from "@prisma/client";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(request: NextApiRequest, response: NextApiResponse) {
-  const { password, email } = request.body;
+// async function handler(request: NextApiRequest, response: NextApiResponse) {
+//   const { password, email } = request.body;
 
-  const user = (await prisma.client.findFirst({ where: { email } })) as Client;
+//   const user = (await prisma.client.findFirst({ where: { email } })) as Client;
 
-  if (user) {
-    
-    const passCompare = await compare(password, user.password);
+//   if (user) {
 
-    if (passCompare) {
-      const token = jwt.sign({}, process.env.SECRET_KEY as string, {
-        expiresIn: "72h",
-        subject: user.id,
-      });
-      return response.status(200).json({ token });
-    }
-  }
-  return response.status(400).json({ message: "Incorrect email or password" });
-}
+//     const passCompare = await compare(password, user.password);
 
-export default handler;
+//     if (passCompare) {
+//       const token = jwt.sign({}, process.env.SECRET_KEY as string, {
+//         expiresIn: "72h",
+//         subject: user.id,
+//       });
+//       return response.status(200).json({ token });
+//     }
+//   }
+//   return response.status(400).json({ message: "Incorrect email or password" });
+// }
+
+// export default handler;
