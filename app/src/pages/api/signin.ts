a9f72403690aeeac28629bc41c465b0e0a80d30b
Leandro <leanndrocabral@gmail.com>
2023-06-05 21:53:30 -0300

    update signin

--- app/src/pages/api/signin.ts
+++ app/src/pages/api/signin.ts
@@ -1,29 +1,31 @@
 import jwt from "jsonwebtoken";
 
 import { compare } from "bcryptjs";
-import { Client } from "@prisma/client";
 import { database } from "../../database/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 async function signIn(request: NextApiRequest, response: NextApiResponse) {
-  const { password, email } = request.body;
+  if (request.method === "POST") {
+    try {
+      const { password, email } = request.body;
 
-  const user = (await database.client.findFirst({
-    where: { email },
-  })) as Client;
+      const user = await database.client.findUniqueOrThrow({
+        where: { email },
+      });
+      await compare(password, user.password);
 
-  if (user) {
-    const passCompare = await compare(password, user.password);
-
-    if (passCompare) {
       const token = jwt.sign({}, process.env.SECRET_KEY as string, {
         expiresIn: "72h",
         subject: user.id,
       });
+
       return response.status(200).json({ token });
+    } catch (error) {
+      return response
+        .status(403)
+        .json({ message: "Incorrect email or password." });
     }
   }
-  return response.status(400).json({ message: "Incorrect email or password." });
 }
 
 export default signIn;
